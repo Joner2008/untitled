09d0cfecff9bb176fd58fb916e40d48b072afaab
Joner2008 <101406401+Joner2008@users.noreply.github.com>
2022-05-20 14:05:55 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,7 @@ input.onButtonPressed(Button.B, function () {
 let meteorito: game.LedSprite = null
 let puntua: game.LedSprite = null
 puntua = game.createSprite(2, 4)
+music.setVolume(127)
 basic.forever(function () {
     basic.pause(randint(1000, 1500))
     meteorito = game.createSprite(randint(0, 4), 0)
@@ -16,9 +17,11 @@ basic.forever(function () {
     }
     if (meteorito.isTouching(puntua)) {
         game.gameOver()
+        music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.Once)
     } else {
         meteorito.delete()
         game.addScore(1)
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Once)
     }
     basic.pause(200)
 })
